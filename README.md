dfc7eb38e3220f4d1e62fcdc01530f41de65b0aa
swimming_kim <tge1375@naver.com>
2021-02-25 13:49:22 +0900

    Update: Format with black / gitignore
    Signed-off-by: swimming_kim <tge1375@naver.com>

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # CarND Introduction to Neural Networks
+
+## Structure
+
+```
+├── README.md
+├── quiz_and_operation.py
+├── quiz_gradient_descent.py
+├── quiz_multilayer_perceptron.py
+├── quiz_not_operation.py
+└── quiz_perceptron
+    ├── data.csv
+    └── perceptron.py
+```
--- quiz_multilayer_perceptron.py
+++ quiz_multilayer_perceptron.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+
 def sigmoid(x):
     """
     Calculate sigmoid
     """
-    return 1/(1+np.exp(-x))
+    return 1 / (1 + np.exp(-x))
+
 
 # Network size
 N_input = 4
@@ -24,11 +26,11 @@ weights_hidden_to_output = np.random.normal(0, scale=0.1, size=(N_hidden, N_outp
 hidden_layer_in = np.dot(X, weights_input_to_hidden)
 hidden_layer_out = sigmoid(hidden_layer_in)
 
-print('Hidden-layer Output:')
+print("Hidden-layer Output:")
 print(hidden_layer_out)
 
 output_layer_in = np.dot(hidden_layer_out, weights_hidden_to_output)
 output_layer_out = sigmoid(output_layer_in)
 
-print('Output-layer Output:')
+print("Output-layer Output:")
 print(output_layer_out)
